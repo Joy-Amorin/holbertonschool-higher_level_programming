33de0ff53efa3c1e0885677525a44f31790969f5
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-22 13:16:32 -0800

    fix output comma and space

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n in range(10):
     for i in range(10):
         if n < i and n < 8:
-            print("{0}{1} ,".format(n,i), end="")
+            print("{0}{1}, ".format(n, i), end="")
         if n == 8 and i == 9:
-            print("{0}{1}".format(n,i))
+            print("{0}{1}".format(n, i))
