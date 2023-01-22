b7e8395d0c4bce2d8b0dfbf36e5ea9d046aba52b
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-22 13:09:51 -0800

    tsk 4-6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n in range(10):
+    for i in range(10):
+        if n < i and n < 8:
+            print("{0}{1} ,".format(n,i), end="")
+        if n == 8 and i == 9:
+            print("{0}{1}".format(n,i))
