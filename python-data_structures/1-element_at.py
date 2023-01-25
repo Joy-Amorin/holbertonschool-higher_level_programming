c3037734eef9e9776b424bbbb6ffc67c3ba5e2e4
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-25 12:48:16 -0800

    fix out put range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in list(my_list):
-        if idx < 0 and idx == idx + 2:
+        if idx < 0 and idx > idx + 1:
             return(None)
         else:
             return(idx + 1)
