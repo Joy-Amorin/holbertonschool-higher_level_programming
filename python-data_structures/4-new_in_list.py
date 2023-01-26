f309c54b9d7ec12b759884a99aa908b1777974d0
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-25 17:34:16 -0800

    remove 'for' cycle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,9 +2,10 @@
 def new_in_list(my_list, idx, element):
 
     new_list = my_list[:]
+
+    if idx < 0:
+        return(new_list)
+    if idx >= len(new_list):
+        return(new_list)
     new_list[idx] = element
-    for i in list(new_list):
-        if idx < 0:
-            return(new_list)
-        if idx >= len(my_list):
-            return(new_list)
+    return(new_list)
