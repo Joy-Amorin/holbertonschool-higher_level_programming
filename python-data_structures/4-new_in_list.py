9c2192acbcc78f68fd86bd9f26bd13c37e19fe8a
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-25 17:24:06 -0800

    fix sign

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     new_list[idx] = element
     for i in list(new_list):
-        if idx > 0:
+        if idx < 0:
             return(new_list)
         if idx >= len(my_list):
             return(new_list)
