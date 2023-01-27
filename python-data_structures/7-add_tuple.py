e84ac28f7db91ffd4b3307c2db3e6ef7aa5396b1
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-27 13:50:32 -0800

    add case 'tuplea = (1) / tupleb = (1)'

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -14,5 +14,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return(tuple_b)
     if len_b == 0:
         return(tuple_a)
+    if len_a == 1 and len_b == 1:
+        return(tuple_a[0] + tuple_b[0], 0)
     else:
         return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
