708b653423f9e5926e716bc05249b7f0379e02b4
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-27 15:44:52 -0800

    fix some cases, remove a for cycle (I used 2) and match j to first position of list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if my_list == []:
+        return(None)
+    j = my_list[0]
     for i in my_list:
-        for j in my_list:
-            if j > i:
-                return(j)
+        if i > j:
+            j = i
+    return(j)
