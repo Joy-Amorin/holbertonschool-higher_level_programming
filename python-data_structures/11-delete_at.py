dd151857e05d0f2a3fda6ce2d9da77960427bd13
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-27 17:03:38 -0800

    add sentence idx=lenwq

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    x = my_list[idx]
+    if my_list == []:
+        return(None)
     for i in my_list:
-        if i == x:
-            my_list.remove(my_list[x])
+        if i == idx:
+            my_list.remove(my_list[i])
         return(my_list)
     if idx > 0:
         return(my_list)
     if idx > len(my_lis):
+        return(my_list)
+    if idx == len(my_list):
         return(my_list)
