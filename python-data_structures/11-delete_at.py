28f2ab3a3cc7213722cc80cf891cc290acbb8f05
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-27 18:13:01 -0800

    fix sign error and add case idx=0

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,13 +2,16 @@
 def delete_at(my_list=[], idx=0):
     if my_list == []:
         return []
+    if idx < 0:
+        return(my_list)
+    if idx > len(my_list):
+        return(my_list)
+    if idx == len(my_list):
+        return(my_list)
+    if idx == 0:
+        return(my_list)
+
     for i in my_list:
         if i == idx:
             my_list.remove(my_list[i])
             return(my_list)
-    if idx > 0:
-        return(my_list)
-    if idx > len(my_lis):
-        return(my_list)
-    if idx == len(my_list):
-        return(my_list)
