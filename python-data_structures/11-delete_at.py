c2dc67febbd3c990d73490d935f2d6b9cd45acb3
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-27 18:29:22 -0800

    add range function

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,15 +3,12 @@ def delete_at(my_list=[], idx=0):
     if my_list == []:
         return []
     if idx < 0:
-        return(my_list)
+        return my_list
     if idx > len(my_list):
-        return(my_list)
+        return my_list
     if idx == len(my_list):
-        return(my_list)
-    if idx == 0:
-        return(my_list)
-
-    for i in my_list:
+        return my_list
+    for i in range(len(my_list)):
         if i == idx:
             my_list.remove(my_list[i])
-            return(my_list)
+            return my_list
