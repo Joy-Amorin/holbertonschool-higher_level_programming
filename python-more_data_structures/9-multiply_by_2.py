3aa8124c4701725dacf6bfee272b3db9a0c0dbf9
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-29 10:00:47 -0800

    tsk 8-9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/busr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    for k in new_dictionary:
+        new_dictionary[k] *= 2
+    return(new_dictionary)
