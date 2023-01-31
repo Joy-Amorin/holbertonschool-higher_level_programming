d181fd60f776fc38c71956557b3dcd0765a1cea2
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-31 11:09:26 -0800

    style

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Create class Square that defines a square."""
+"""Create class Square that defines a square.
+"""
+
 
 class Square:
-"""Square that defines a square by 
-private instance attribute ."""
+    """Square that defines a square by
+    private instance attribute ."""
 
     def __init__(self, size):
         self.__size = size
