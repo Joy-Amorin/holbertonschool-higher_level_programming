1bc16a86bb78599484d9ce229413b1546c6f312a
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-31 11:36:22 -0800

    module identation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-class Square:
-"""Write a class Square that defines a square.
+"""Write a class Square that defines a square
 """
+
+
+class Square:
+
     def __init__(self, size=0):
 
         """Write a class Square that defines a square by
