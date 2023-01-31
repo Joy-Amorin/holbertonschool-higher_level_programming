2d497df63137d67cf0959a1122b248a48ab7c954
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-31 10:37:21 -0800

    first commit

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/Python3
+"""Create Square class."""
+class Square:
+"""Create empty Square class."""
+    pass
---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+"""Write a class Square that defines a square """
+    def __init__(self, size=0):
+"""Write a class Square that defines a square by
+private attribuete"""
+        self.__size = 3
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+           raise ValueError("size must be an integer")
