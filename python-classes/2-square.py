22bc28542075490d2307320f6bff982ef2dea9b6
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-31 11:29:06 -0800

    style

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 class Square:
-"""Write a class Square that defines a square """
+    """Write a class Square that defines a square.
+    """
     def __init__(self, size=0):
-"""Write a class Square that defines a square by
-private attribuete"""
+
+        """Write a class Square that defines a square by
+        private attribuete"""
         self.__size = 3
 
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
-           raise ValueError("size must be an integer")
+            raise ValueError("size must be an integer")
