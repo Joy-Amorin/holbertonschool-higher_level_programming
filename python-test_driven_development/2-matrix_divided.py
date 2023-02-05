8f583005cad8c838c886555a94ff06ccb81c5ac5
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-05 15:19:49 -0800

    fix strings messages

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,12 +16,12 @@ def matrix_divided(matrix, div):
         if i + 1 < len(matrix):
             if len(matrix[i]) != len(matrix[i + 1]):
                 raise TypeError("Each row of the matrix \
-                                must have the same size")
+must have the same size")
 
         for j in range(len(matrix[i])):
             if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
                 raise TypeError("matrix must be a matrix \
-                                (list of lists) of integers/floats")
+list of lists) of integers/floats")
 
             list_matrix[i][j] = round(list_matrix[i][j] / div, 2)
     """'round' function for rounding decimal numbers """
