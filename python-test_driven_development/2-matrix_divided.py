db2f587e4c6e7aaa8d963a5ba5b4f9d2c4e12d8f
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-05 15:07:18 -0800

    tsk_1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """function to divided elements"""
+
+    list_matrix = list(map(list, matrix))
+
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for i in range(len(matrix)):
+        if i + 1 < len(matrix):
+            if len(matrix[i]) != len(matrix[i + 1]):
+                raise TypeError("Each row of the matrix \
+                                must have the same size")
+
+        for j in range(len(matrix[i])):
+            if type(matrix[i][j]) != int and type(matrix[i][j]) != float:
+                raise TypeError("matrix must be a matrix \
+                                (list of lists) of integers/floats")
+
+            list_matrix[i][j] = round(list_matrix[i][j] / div, 2)
+    """'round' function for rounding decimal numbers """
+
+    return (list_matrix)
