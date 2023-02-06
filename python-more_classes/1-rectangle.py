2b252f21a2f84608b10a53ca597b731587fe271b
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-06 10:14:05 -0800

    fix string message

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
