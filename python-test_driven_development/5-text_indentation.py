cc29bc9ceea0063c2c22a21d919bc78e295c7314
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-05 18:25:18 -0800

    5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""printa text with 2 new lines after each of 
+these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """function that prints"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        if text[i] in {'.', '?', ':'}:
+            print(text[i], end="")
+            print("\n\n", end="")
+            while text[i + 1] == ' ':
+                i += 1
+        else:
+            print(text[i], end="")
+        i += 1
