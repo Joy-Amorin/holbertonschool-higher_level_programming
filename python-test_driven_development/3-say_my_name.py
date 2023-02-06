fa58de598fc58e6c9ff2ce7471b9f83e58384989
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-05 16:07:28 -0800

    3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""print My name is <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that prints name"""
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
