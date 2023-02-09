a059a8fe35443e74cbba5dc68de85b733ca1c5cd
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-09 14:30:34 -0800

    tsks_8_9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""write BaseGeometry empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+    def area(self):
+        """area """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value """
+
+        if type(value) != int:
+            raise TypeError("<name> must be an integer>")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        self.name = value
+
+
+class Rectangle(BaseGeometry):
+    """class  inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        """define attributes width, height"""
+
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", height)
+        self.integer_validator("height", width)
+
+    def area(self):
+        return(self.__width * self.__height)
+
+    def __str__(self):
+        return(f"[Rectangle] {self.__width}/{self.__height}")
