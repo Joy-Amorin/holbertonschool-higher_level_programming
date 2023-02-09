f2daf1e71d7f6b3510e753ea769e5d0045a560d3
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-09 09:33:01 -0800

    tsks_5_6

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/p#!/usr/bin/python3
+"""write BaseGeometry empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""write BaseGeometry empty class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+    def area(self):
+        """area """
+        raise Exception("area() is not implemented")
