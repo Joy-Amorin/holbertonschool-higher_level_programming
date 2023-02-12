782d0e69eaf8056ea097700b3ae5e535d7c871fb
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-12 11:44:33 -0800

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""write Square class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        BaseGeometry.integer_validator(self, "size", size)
+        self.__size = size
+
+    def area(self):
+        """return area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
