c7984008d1636a1c5d1925846933069620aa4001
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-15 05:33:56 -0800

    tsk_4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
 
         self.__y = value
+
+    def area(self):
+        """return area self"""
+        return(self.__width * self.__height)
