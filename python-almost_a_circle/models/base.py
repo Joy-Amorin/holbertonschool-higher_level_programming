8defcca12201928876f4430d6deacf5b276b1d23
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-18 10:55:02 -0800

    tsk_15 module documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,8 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """returns the JSON string representation
+        of list_dictionaries"""
 
         if list_dictionaries is None:
             return("[]")
