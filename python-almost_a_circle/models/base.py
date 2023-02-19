f1220820cd1374e1170429bf55744e8035031aa3
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-02-19 08:39:03 -0800

    tsk_18_19

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Base class"""
 import json
+from os import path 
 
 
 class Base:
@@ -47,3 +48,26 @@ class Base:
             return([])
         else:
             return(json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all
+        attributes already set"""
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances"""
+        if not path.exists(f"{cls.__name__}.json"):
+            return []
+        with open(f"{cls.__name__}.json", encoding='utf-8') as f:
+            list_instance = []
+            for dicts in cls.from_json_string(f.read()):
+                list_instance.append(cls.create(**dicts))
+        return list_instance
